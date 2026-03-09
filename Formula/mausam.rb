9b2dec521abbe6d75f53d169f391b71783fe6a19
Bhanu <bhanu9112002@gmail.com>
2026-03-09 12:44:58 +0530

    fix: add sha256 checksum to Homebrew formula

--- Formula/mausam.rb
+++ Formula/mausam.rb
@@ -2,6 +2,7 @@ class Mausam < Formula
   desc "Beautiful weather in your terminal"
   homepage "https://github.com/codeptor/mausam"
   url "https://github.com/codeptor/mausam/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "1f79838cf217f0144c9c61e6e0c92afcdcda1e564ae686b6f7877ab9672e40ca"
   license "MIT"
 
   depends_on "rust" => :build
