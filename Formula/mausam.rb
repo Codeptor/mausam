878dfe36144415deb86d1c1f03b07b095d0a562d
Bhanu <bhanu9112002@gmail.com>
2026-03-10 21:25:27 +0530

    release: v1.0.4

--- Formula/mausam.rb
+++ Formula/mausam.rb
@@ -1,8 +1,8 @@
 class Mausam < Formula
   desc "Beautiful weather in your terminal"
   homepage "https://github.com/codeptor/mausam"
-  url "https://github.com/codeptor/mausam/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "03966f9dbf3666249ab0aa46446bc40a16202ee3e748d5e24d4eb3c45a375df0"
+  url "https://github.com/codeptor/mausam/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 ""
   license "MIT"
 
   depends_on "rust" => :build
